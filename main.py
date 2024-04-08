6a805bed35c07bda3dba8a2fadda48b3df4d7272
atul kumar singh <atulsingh9507@gmail.com>
2024-04-08 12:53:39 +0530

    FASTAPI

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+
+from pymongo import MongoClient
+
+app = FastAPI()
+
+
+conn = MongoClient("mongodb://localhost:27017")
+
+
+
